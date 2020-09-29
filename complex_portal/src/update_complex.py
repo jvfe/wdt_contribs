adc19549c4238dc501e3c9e76dd4a1b811415fcc
jvfe <jvfecav@gmail.com>
2020-09-29 20:34:57 -0300

    refactor: Improving data processing
    Also better comments in the data processing section, to not alienate
    contributors.

--- complex_portal/src/update_complex.py
+++ complex_portal/src/update_complex.py
@@ -13,27 +13,35 @@ def process_species_complextab(complextab_dataframe):
     """Clean and process complextab data
 
     Takes in a complextab dataframe from one of the species datasets,
-    remove entries present in Wikidata and processes it into a "long"
+    removes entries present in Wikidata and processes it into a "long"
     format, more friendly for editing.
 
     """
     species_missing_raw = return_missing_from_wikidata(complextab_dataframe)
+
+    # Cleaning molecules column, they follow this format: uniprot_id(quantity)|another_uniprot_id(n)...
     molecules_column = "Identifiers (and stoichiometry) of molecules in complex"
+
     species_missing_raw[molecules_column] = species_missing_raw[
         molecules_column
     ].str.split("|")
+
     species_missing_raw = species_missing_raw.explode(molecules_column)
+
     species_missing_raw["has_part_quantity"] = species_missing_raw[
         molecules_column
     ].str.extract(r"\(([\d]+)\)", expand=False)
+
     species_missing_raw["uniprot_id"] = species_missing_raw[
         molecules_column
     ].str.replace(r"\(.*\)", "")
+
+    # Also need to group the resulting molecules, to avoid duplicates
     species_missing = (
         species_missing_raw.groupby(
             ["#Complex ac", "Recommended name", "Taxonomy identifier", "uniprot_id"]
         )
-        .agg(part_of_quantity=pd.NamedAgg("has_part_quantity", "count"))
+        .agg(has_part_quantity=pd.NamedAgg("has_part_quantity", "count"))
         .reset_index()
     )
 
@@ -44,10 +52,16 @@ def process_species_complextab(complextab_dataframe):
 cov2_complextab = pd.read_table(datasets["sars-cov-2"], na_values=["-"])
 cov2 = process_species_complextab(cov2_complextab)
 cov2["found_in_taxon"] = [
-    get_wikidata_item_by_propertyvalue("P685", taxid)
+    get_wikidata_item_by_propertyvalue("P685", int(taxid))
     for taxid in cov2["Taxonomy identifier"].to_list()
 ]
 cov2["has_part"] = [
     get_wikidata_item_by_propertyvalue("P352", uniprot_id)
     for uniprot_id in cov2["uniprot_id"].to_list()
+]
+
+# %%
+complex_dfs = [
+    cov2[cov2["#Complex ac"] == unique_complex]
+    for unique_complex in cov2["#Complex ac"].unique()
 ]

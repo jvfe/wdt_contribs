080c5cc2b67af3449aeb8dfd2986b2b9eae7c4ec
jvfe <jvfecav@gmail.com>
2020-09-30 17:01:04 -0300

    Add WDI section for the actual edits (not done)

--- complex_portal/src/update_complex.py
+++ complex_portal/src/update_complex.py
@@ -1,4 +1,6 @@
 # %%
+from wikidataintegrator import wdi_core, wdi_login
+from time import gmtime, strftime
 import pandas as pd
 from utils import (
     get_complex_portal_datasets,
@@ -61,7 +63,38 @@ cov2["has_part"] = [
 ]
 
 # %%
+# Make a dataframe for each unique complex
 complex_dfs = [
-    cov2[cov2["#Complex ac"] == unique_complex]
+    cov2[cov2["#Complex ac"] == unique_complex].reset_index()
     for unique_complex in cov2["#Complex ac"].unique()
 ]
+# %%
+# login_instance = wdi_login.WDLogin(user='<bot user name>', pwd='<bot password>')
+
+stated_in = wdi_core.WDItemID(value="Q47196990", prop_nr="P248", is_reference=True)
+wikidata_time = strftime("+%Y-%m-%dT00:00:00Z", gmtime())
+retrieved = wdi_core.WDTime(wikidata_time, prop_nr="P813", is_reference=True)
+references = [stated_in, retrieved]
+
+for df in complex_dfs:
+
+    current_complex = df["#Complex ac"].unique()[0]
+
+    instance_of = wdi_core.WDItemID(value="Q22325163", prop_nr="P31")
+
+    found_in_taxon = wdi_core.WDItemID(value=df["found_in_taxon"][0], prop_nr="P703")
+
+    complexportalid = wdi_core.WDString(
+        value=current_complex, prop_nr="P7718", references=references
+    )
+
+    data = [instance_of, found_in_taxon, complexportalid]
+
+    has_parts = [
+        wdi_core.WDItemID(value=protein, prop_nr="P703") for protein in df["has_part"]
+    ]
+
+    data.extend(has_parts)
+
+    # wd_item = wdi_core.WDItemEngine(data=data)
+    # wd_item.write(login_instance)
